0c963b76fe204976e73ec354a2129acd9b7af0c6
ConsuL <consul.developer@gmail.com>
2021-08-23 15:55:15 +0300

    code style

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -16,17 +16,19 @@ class PaymentsController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-           'email' => 'required|email',
-           'amount' => 'required|integer|min:100',
+            'email' => 'required|email',
+            'amount' => 'required|integer|min:100',
+            'currency' => 'required',
+            'name' => 'required',
         ]);
 
         $request->user()->payments()->create([
-            'amount'=> $request->amount,
-            'currency'=> $request->currency,
-            'email'=> $request->email,
-            'name'=> $request->name,
-            'description'=> $request->description,
-            'message'=> $request->message,
+            'amount' => $request->amount,
+            'currency' => $request->currency,
+            'email' => $request->email,
+            'name' => $request->name,
+            'description' => $request->description,
+            'message' => $request->message,
         ]);
     }
 }
